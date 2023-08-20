90bddd79bc6b1def76cbe242159165c5d76bf608
tamcony <tamcony@icloud.com>
2023-08-20 14:45:16 +0800

    🐛 修改部分样式

--- src/TUIKit/TUIComponents/container/TUIChat/index.vue
+++ src/TUIKit/TUIComponents/container/TUIChat/index.vue
@@ -17,12 +17,12 @@
       </aside>
     </header>
     <div class="TUIChat-main">
-      <div class="TUIChat-safe-tips">
+      <!-- <div class="TUIChat-safe-tips">
         <span>
           {{ $t('TUIChat.安全提示') }}
         </span>
         <a @click="openLink(Link.complaint)">{{ $t('TUIChat.点此投诉') }}</a>
-      </div>
+      </div> -->
       <ul class="TUI-message-list" @click="dialogID = ''" ref="messageEle" id="messageEle">
         <p class="message-more" @click="getHistoryMessageList" v-if="!isCompleted">
           {{ $t('TUIChat.查看更多') }}
@@ -878,4 +878,6 @@ const TUIChat: any = defineComponent({
 export default TUIChat;
 </script>
 
-<style lang="scss" scoped src="./style/index.scss"></style>
+<style lang="scss" scoped src="./style/index.scss">
+.
+</style>
